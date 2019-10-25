7d436ecabfe3f649d0224ee1cfe73687a9053447
Niyaz <niyazm524@gmail.com>
2019-10-25 22:26:16 +0300

    [FIX] Character encoding set to UTF-8 to support the whole project russian letters

--- src/Sample.java
+++ src/Sample.java
@@ -11,7 +11,7 @@ public class Sample extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try(PrintWriter writer = resp.getWriter()) {
-            writer.println("Nado delat..");
+            writer.println("Надо делать..");
         }
     }
 }
